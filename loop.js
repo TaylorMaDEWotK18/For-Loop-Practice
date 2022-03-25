d7b312a1db25886b9c673f2af87dff6dac01971b
TaylorMade <warriorotk18@gmail.com>
2022-03-25 00:30:00 -0600

    Final notes and function working with for loop

--- loop.js
+++ loop.js
@@ -1,6 +1,6 @@
-for (let x = 0; x < 10; x += 1 ) {
-        console.log(x);
-}
+// for (let x = 0; x < 10; x += 1 ) {
+//         console.log(x);
+// }
 
 function getRandomNumber(upper) {
     return Math.floor( Math.random() * upper ) + 1;
@@ -14,10 +14,10 @@ function getRandomNumber(upper) {
 // 2) Next comes the condition. The condition gets evaluated BEFORE the loop runs each time
 //     - ( x = 0; x < 10)
 
-// 3) The last component of the for loop updates the counter each time through the loop. This counter gets evaluated and updated at the end of each loop iteration.
-    // - ( x = 0; x < 10; x += 1)
+// 3) The last component of the for loop updates the counter each time through the loop. This counter gets evaluated and updated at the end of each loop iteration. 
+    // - ( x = 0; x < 10; x += 1) (OR x++(increment operator))
 
 // 4) Finally inside the code block is the code that runs each time the condition `( x < 10 )` evaluates TRUE
-    for ( x = 0; x < 10; x += 1(OR x++(increment operator))) {
-        console.log(`The ramdom number is ${getRandomNumber(10)}`);
+    for ( x = 0; x < 10; x += 1) {
+        console.log(`The ramdom number is ${getRandomNumber(100)}`);
     }
